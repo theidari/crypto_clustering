972588db57954d168eab43c7762f2b94958b2404
Ali Taghipour Heidari <theidari.ali@gmail.com>
2023-03-21 23:36:35 -0400

    before cleaning

--- src/package/constants.py
+++ src/package/constants.py
@@ -1,56 +1,43 @@
-# --------------------------------------------------------------------------------------------------------
-# ---------------------------------------------- Constants------------------------------------------------
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+#                                 CONSTANTS
+# --------------------------------------------------------------------------------------------
 
-# 1. folders and files name -----------------------------------------------------------------------------/
+# Folders and files names
+DATA_URL = "../data/"
+OUTPUT_URL = "../Output/"
 
-lng                 = "longitude"
-lat                 = "latitude"
-time_length         = "12 month"         
-tobs                = "Temperatures Observed"
-station             = "Station"
-freq                = "Frequency"
-prcp                = "Precipitation"
-MAP_NAME            = "Honolulu"
-# -------------------------------------------------------------------------------------------------------/
+# Station location (station_location.py) constants
+SESSION_COLUMN_NAMES = ["station", "name", "latitude", "longitude", "elevation", "number_of_collected_data"]
 
-# 2. defining directories -------------------------------------------------------------------------------/
-db_path             = "sqlite:///../Resources/hawaii.sqlite"
-output_url          = "../Output/"
-# -------------------------------------------------------------------------------------------------------/
+# Plot constants
+# Map plot constants
+MAP_WIDTH = 550
+MAP_HEIGHT = 475
+MAP_TOOLS = "hover"
+MARKER_SHAPE = "triangle_dot"
+MARKER_SIZE = 15
+MARKER_FILL_COLOR = "red"
+MARKER_FILL_ALPHA = 0.5
+MARKER_LINE_COLOR = "black"
+MARKER_LINE_ALPHA = 0.8
 
-# 3. station location (station_location.py) constants ---------------------------------------------------/
-SESSION_COLUMN_NAME = ["station", "name", "latitude", "longitude", "elevation", "number_of_colected_data"]
+# Histogram and line plot constants
+FIG_HEIGHT = 8
+FIG_WIDTH = 8
+TITLE_FONT_SIZE = 15
+LABEL_FONT_SIZE = 12
+TICKS_FONT_SIZE = 10
+LEGEND_FONT_SIZE = 10
+FONT_WEIGHT = "bold"
+PLOT_COLOR = "tab:blue"
+LEGEND_LOCATION = "best"
+HIS_X_LABEL = "Temperature"
+HIS_Y_LABEL = "Frequency"
+LINE_X_LABEL = "Date (yyyy-mm-dd)"
+LINE_Y_LABEL = "Inches"
+LINE_WIDTH = 1 
 
-# 4. plot -----------------------------------------------------------------------------------------------/
-
-# a-4) map plot constants
-
-MAP_WEIGTH          = 550
-MAP_HEIGHT          = 475
-MAP_TOOLS           = "hover"
-MARKER_SHAPE        = "triangle_dot"
-MARKER_SIZE         = 15
-MARKER_FILL_COLOR   = "red"
-MARKER_FILL_ALPHA   = 0.5
-MARKER_LINE_COLOR   = "black"
-MARKER_LINE_ALPHA   = 0.8
-
-# b-4) histogram and line constants
-title_font_size     = 15
-label_font_size     = 12
-ticks_font_size     = 10
-legend_font_size    = 10
-font_weight         = "bold"
-plt_fig_higth       = 10
-plt_fig_width       = 8
-plt_color           = "tab:blue"
-legend_location     = "best"
-his_x_label         = "Temperature"
-his_y_label         = "Frequency"
-line_x_label        = "Date (yyyy-mm-dd)"
-line_y_label        = "Inches"
-line_width          = 1 
-# -------------------------------------------------------------------------------------------------------/
-
-
+# Colors
+SEVENSET=["#ffd700","#ffb14e","#fa8775","#ea5f94","#cd34b5","#9d02d7","#0000ff"]
+FOURSET=1
+FIVESET=2
--- src/package/helpers.py
+++ src/package/helpers.py
@@ -2,90 +2,111 @@
 # -------------------- All libraries, variables and functions are defined in this fil --------------------
 # --------------------------------------------------------------------------------------------------------
 
-# 1. libraries ------------------------------------------------------------------------------------------/
-from package_1.constants import * # constants
-# a-1) main dependencies and setup
-import warnings
-warnings.filterwarnings("ignore")
-import numpy as np
+
 import pandas as pd
-import datetime as dt
-from threading import Timer
-import webbrowser
+
+import hvplot.pandas
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
+from sklearn.cluster import KMeans
+
+import plotly.graph_objects as go
+
+# 1. libraries ------------------------------------------------------------------------------------------/
+# a-1) main dependencies and setup
+from package.constants import * # constants
+
 
 # b-1) plotting
-from matplotlib import style
-style.use('fivethirtyeight')
-import matplotlib.pyplot as plt
-import hvplot.pandas
-import geoviews as gv
-import geoviews.feature as gf
-from geoviews import dim, opts
-import geoviews.tile_sources as gts
-gv.extension('bokeh')
+
 # -------------------------------------------------------------------------------------------------------/
 
-# 2. function definition --------------------------------------------------------------------------------/
-# a-2) station location plotting function (map)
-def station_location(df, kdims): # df = DataFrame # variable column
-    station_location = gv.Dataset(df, kdims=[kdims])
-    locs = station_location.to(gv.Points, [lng, lat], ["station", "elevation", "number_of_colected_data"])
-    locs_plot=(gts.OSM  * locs).opts(
-        opts.Points(
-        width      = MAP_WEIGTH,
-        height     = MAP_HEIGHT,
-        tools      = [MAP_TOOLS],
-        marker     = MARKER_SHAPE,
-        size       = MARKER_SIZE,
-        fill_color = MARKER_FILL_COLOR, 
-        fill_alpha = MARKER_FILL_ALPHA,
-        line_color = MARKER_LINE_COLOR,    
-        line_alpha = MARKER_LINE_ALPHA
-        ))
-    gv.save(locs_plot, output_url+MAP_NAME+".html")
-    return locs_plot
+# Plotting function --------------------------------------------------------------------------------/
+def line (df, chart_title):
+    # Create a list of traces for each column in the DataFrame
+    traces = []
+    for i, col in enumerate(df.columns):
+        col_name = col.split("_")
+        trace = go.Scatter(x=df.index,
+                           y=df[col],
+                           name=col_name[-1],
+                           mode='lines',
+                           line=dict(color=SEVENSET[i%len(SEVENSET)]),
+                          )
+        traces.append(trace)
+    # Create the layout
+    layout = go.Layout(title=dict(text=chart_title,
+                                  font=dict(size= 24, color= 'black', family= "Times New Roman"),
+                                  x=0.5,
+                                  y=0.9),
+                       width=1000,
+                       height=600,
+                       legend=dict(
+                           yanchor="top",
+                           y=0.99,
+                           xanchor="left",
+                           x=0.01,
+                           bgcolor= '#f7f7f7',
+                           font=dict(color='black')),
+                       xaxis=dict(title='Crypto',
+                                  color= 'black',
+                                  showline=True,
+                                  linewidth=1,
+                                  linecolor='black',
+                                  mirror=True), 
+                       yaxis=dict(title='Price Change (%)',
+                                  color= 'black',
+                                  showline=True,
+                                  linewidth=1,
+                                  linecolor='black',
+                                  mirror=True),
+                       plot_bgcolor='#f7f7f7',
+                       paper_bgcolor="#f7f7f7")
 
-# a-3) histogram plot
+    # Create the figure
+    fig = go.Figure(data=traces, layout=layout)
+    # Show the figure
+    fig.show()
+
 def histogram (df, bins, location):
-    # plot size
-    plt.figure(figsize=(plt_fig_higth,plt_fig_width))
-    # plot main describtion
-    plt.hist(np.array(df), color = plt_color, bins=bins)
-    # ticks definitions
-    plt.yticks(size=ticks_font_size)
-    plt.xticks(size=ticks_font_size)
-    # labels definitions
-    plt.xlabel(his_x_label,fontsize=label_font_size)
-    plt.ylabel(his_y_label,fontsize=label_font_size)
-    # title definitions
-    plt.title(tobs+" at "+location+" in "+time_length, fontsize=title_font_size, fontweight = font_weight, pad=+20)
-    # legend definitions
-    plt.legend([tobs], loc = legend_location, fontsize=legend_font_size)
-    # plt save
-    plt.savefig(output_url+tobs+"_at_"+location+"_in_"+time_length+".png",dpi=300, bbox_inches='tight')
-    return plt.show()
+    # Set the figure size
+    plt.figure(figsize=(FIG_HEIGHT, FIG_WIDTH))
 
-# a-3) line graph
-def line (df,date_in,date_out, location):
-    # data length
-    length=len(df.index)
-    # plot size
-    plt.figure(figsize=(plt_fig_higth,plt_fig_width))
-    # plot main describtion
-    plt.plot(df, color = plt_color, linewidth=line_width)
-    # ticks definitions
-    plt.xticks(np.arange(0,length,50), rotation = 90, rotation_mode = 'anchor',ha = 'right', fontsize=ticks_font_size)
-    plt.yticks(fontsize=ticks_font_size)
-    # lim definitions
-    plt.xlim(date_in,date_out)
-    # label definitions
-    plt.xlabel(line_x_label, fontsize=label_font_size)
-    plt.ylabel(line_y_label, fontsize=label_font_size)
-    # title definitions
-    plt.title(prcp+" in "+location+" from "+date_in+" to "+date_out, fontsize=title_font_size, fontweight = font_weight, pad=+20)
-    # legend definitions
-    plt.legend([prcp], loc = legend_location, fontsize=legend_font_size)
-    # plt save
-    plt.savefig(output_url+prcp+"_in_"+location+"_from_"+date_in+"_to_"+date_out+".png",dpi=300, bbox_inches='tight')
-    return plt.show()
+    #Plot the Clusters
+    ax = sns.scatterplot(data = df_market_scaled,
+                         x = 'price_change_percentage_24h',
+                         y = 'price_change_percentage_7d', 
+                         hue = km.labels_, 
+                         palette = 'colorblind', 
+                         alpha = 0.8, 
+                         s = 150,
+                         legend = False)
+
+    #Plot the Centroids
+    ax = sns.scatterplot(data = cluster_centers, 
+                         x = 'price_change_percentage_24h',
+                         y = 'price_change_percentage_7d', 
+                         hue = cluster_centers.index, 
+                         palette = 'colorblind', 
+                         s = 600,
+                         marker = 'D',
+                         ec = 'black', 
+                         legend = False)
+
+    # Add Centroid Labels
+    for i in range(len(cluster_centers)):
+                   plt.text(x = cluster_centers.price_change_percentage_24h[i], 
+                            y = cluster_centers.price_change_percentage_7d[i],
+                            s = i, 
+                            horizontalalignment='center',
+                            verticalalignment='center',
+                            size = 15,
+                            weight = 'bold',
+                            color = 'white')
 # -------------------------------------------------------------------------------------------------------/
